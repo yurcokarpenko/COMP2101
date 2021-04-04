32306307d88369f6e5a35398371265134dd1f7fb
yurcokarpenko <74422688+yurcokarpenko@users.noreply.github.com>
2021-04-03 21:07:50 -0400

    lab3 done
    Lab3 done

---
+++ powershell/get-cpuinfo.ps1
@@ -0,0 +1,16 @@
+﻿function get-cpuinfo {
+>> "Manufacturer :";
+>> (get-ciminstance cim_processor).manufacturer;
+>>
+>> "Name :";
+>> (get-ciminstance cim_processor).Name;
+>>
+>> "Current Speed :"
+>> (get-ciminstance cim_processor).currentclockspeed;
+>>
+>> "Maximum Clock Speed :";
+>> (get-ciminstance cim_processor).maxclockspeed;
+>>
+>> "Number of Cores for Each Proccesor in This System :"
+>> (get-ciminstance cim_processor).numberofcores;
+>> }
---
+++ powershell/welcome.ps1
@@ -0,0 +1,6 @@
+﻿funtion welcome {
+
+    write-output "Welcome to planet $env:computername Overlord $env:username"
+    $now = get-date -format 'HH:MM tt on dddd'
+    write-output "It is $now."
+    }
