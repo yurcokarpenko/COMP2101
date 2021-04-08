e5a0f2c5f916b5bff33d0a39ffea4e74c4522f44
yurcokarpenko <74422688+yurcokarpenko@users.noreply.github.com>
2021-04-08 12:34:38 -0400

    lab3

--- powershell/Lab3/lab3.ps1
+++ powershell/Lab3/lab3.ps1
@@ -1,9 +1,10 @@
 ﻿get-ciminstance win32_networkadapterconfiguration |
     Where-Object IPEnabled -eq "True" |
-        Select-Object -Property @{n="Index";e={$_.index}},
+        Select-Object -Property @{n="Adapter Description";e={$_.description}},
+				@{n="Index";e={$_.index}},
                                 @{n="IP Address";e={$_.ipaddress}},
                                 @{n="Subnet Mask";e={$_.ipsubnet}},
                                 @{n="DNS Domain Name";e={$_.dnshostname}},
                                 @{n="DNS Server"; e={$_.DNSServerSearchOrder}} |
-Format-Table
+Format-Table -autosize
   
